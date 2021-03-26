46e982a81c158fc620750a758b955c4dc6e58726
yinghan <t_linyh@channelsoft.com>
2021-03-26 20:31:27 +0800

    window提交

--- js基础/test.js
+++
@@ -1,5 +0,0 @@
-Number.prototype.b="haha";
-let a=12;
-//a.b="haha";
-console.log(a.b);
-console.log(typeof(a))
---
+++ letcode/second/868.二进制间距.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=868 lang=javascript
+ *
+ * [868] 二进制间距
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var binaryGap = function(n) {
+    let count = 0,
+        n = n & 1;
+    while(n >> 1){
+        
+    }
+};
+// @lc code=end
+
