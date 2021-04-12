014494416651deb65ae123754d01e8058b5cc4df
anne-lin <yinghan.lin@qq.com>
2021-04-12 20:12:09 +0800

    mac

---
+++ letcode/second/5.最长回文子串.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let dp = [{
+        str: s[0],
+        start: 0
+    }];
+    for (let i = 1; i < s.length; i++){
+        if(dp[i-1].start == 0 )
+    }
+};
+// @lc code=end
+
---
+++ letcode/second/dist/5.最长回文子串.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+ * @lc app=leetcode.cn id=5 lang=javascript
+ *
+ * [5] 最长回文子串
+ */
+// @lc code=start
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function longestPalindrome(s) {
+  var dp = [{
+    str: s[0],
+    start: 0
+  }];
+
+  for (var i = 1; i < s.length; i++) {}
+}; // @lc code=end
